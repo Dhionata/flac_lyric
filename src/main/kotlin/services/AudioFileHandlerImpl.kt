9a1cf1117874850952f15cbdd6e09a37cb95e695
Dhionatã <xiyunagi@gmail.com>
2024-08-26 20:05:17 -0300

    getAudioFiles() agora não usa variáveis intermediárias e faz o retorno da chamada de getFilesByExtension() de fileService.

--- src/main/kotlin/services/AudioFileHandlerImpl.kt
+++ src/main/kotlin/services/AudioFileHandlerImpl.kt
@@ -4,13 +4,11 @@ import interfaces.AudioFileHandler
 import interfaces.FileService
 import java.io.File
 
-class AudioFileHandlerImpl(private val fileService: FileService) : AudioFileHandler {
+class AudioFileHandlerImpl(private val fileService: FileService = FileServiceImpl()) : AudioFileHandler {
 
     private val supportedExtensions = listOf("flac", "mp3", "ogg", "wav", "m4a")
 
     override fun getAudioFiles(musicDirectory: File): Set<File> {
-        val audioFilesSet = mutableSetOf<File>()
-        audioFilesSet.addAll(fileService.getFilesByExtension(musicDirectory, supportedExtensions))
-        return audioFilesSet
+        return fileService.getFilesByExtension(musicDirectory, supportedExtensions)
     }
 }
