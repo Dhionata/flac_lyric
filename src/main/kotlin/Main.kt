38616835668615676193f3adfba9ebee5cfab86e
Dhionatã <xiyunagi@gmail.com>
2024-08-26 20:01:56 -0300

    Adicionado estrutura try/catch e remoção dos valores de parâmetro para MusicLyricsService(), este agora detém as implementações padrões.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,20 +1,10 @@
-import services.AudioFileHandlerImpl
-import services.DirectoryServiceImpl
-import services.FileServiceImpl
-import services.LyricFileHandlerImpl
 import services.MusicLyricsService
 import ui.UserInterfaceImpl
 
 fun main() {
-    val fileService = FileServiceImpl()
-    val directoryService = DirectoryServiceImpl(fileService)
-    val audioFileHandler = AudioFileHandlerImpl(fileService)
-    val lyricFileHandler = LyricFileHandlerImpl(fileService)
-    val userInterface = UserInterfaceImpl()
-
-    val musicLyricsService = MusicLyricsService(
-        directoryService, audioFileHandler, lyricFileHandler, userInterface
-    )
-
-    musicLyricsService.organizeMusicAndLyrics()
+    try {
+        MusicLyricsService().organizeMusicAndLyrics()
+    } catch (e: Exception) {
+        UserInterfaceImpl().showError(e.toString())
+    }
 }
