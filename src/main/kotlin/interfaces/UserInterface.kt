12a885e27ed0356d68d9cf50e3a364e663e000eb
Dhionatã <xiyunagi@gmail.com>
2024-08-26 19:56:12 -0300

    Adição do método move(); Alteração de showResult para usar JScrollPane, bem como uma função aninhada showMessageDialog() para evitar repetição de código.

--- src/main/kotlin/interfaces/UserInterface.kt
+++ src/main/kotlin/interfaces/UserInterface.kt
@@ -1,6 +1,9 @@
 package interfaces
 
+import models.FilePair
+
 interface UserInterface {
     fun showError(message: String)
     fun showResult(changedSet: Set<String>, errorList: Set<Exception>)
+    fun move(filePairs: FilePair): Boolean
 }
--- src/main/kotlin/ui/UserInterfaceImpl.kt
+++ src/main/kotlin/ui/UserInterfaceImpl.kt
@@ -1,29 +1,68 @@
 package ui
 
 import interfaces.UserInterface
+import models.FilePair
+import java.awt.Dimension
 import javax.swing.JOptionPane
+import javax.swing.JScrollPane
+import javax.swing.JTextArea
+import javax.swing.UIManager
+import kotlin.system.exitProcess
 
 class UserInterfaceImpl : UserInterface {
+
+    init {
+        UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName())
+    }
 
     override fun showError(message: String) {
         JOptionPane.showMessageDialog(null, message, "Erro", JOptionPane.ERROR_MESSAGE)
     }
 
     override fun showResult(changedSet: Set<String>, errorList: Set<Exception>) {
+        fun showMessageDialog(content: String, title: String, messageType: Int) {
+            val textArea = JTextArea(content).apply {
+                isEditable = false
+                lineWrap = true
+                wrapStyleWord = true
+            }
+            val scrollPane = JScrollPane(textArea).apply {
+                preferredSize = Dimension(500, 300)
+            }
+            JOptionPane.showMessageDialog(null, scrollPane, title, messageType)
+        }
+
         if (errorList.isNotEmpty()) {
             val errorMessage = errorList.joinToString("\n") { it.message ?: it.toString() }
-            JOptionPane.showMessageDialog(null, errorMessage, "Erros", JOptionPane.ERROR_MESSAGE)
+            showMessageDialog(errorMessage, "Erros", JOptionPane.ERROR_MESSAGE)
         }
 
         if (changedSet.isNotEmpty()) {
             val movedMessage = changedSet.joinToString("\n") { it }
-            JOptionPane.showMessageDialog(null, movedMessage, "Informação", JOptionPane.INFORMATION_MESSAGE)
+            showMessageDialog(movedMessage, "Informação", JOptionPane.INFORMATION_MESSAGE)
         }
 
         if (errorList.isEmpty() && changedSet.isEmpty()) {
-            JOptionPane.showMessageDialog(
-                null, "Está tudo no lugar!", "Informação", JOptionPane.INFORMATION_MESSAGE
-            )
+            JOptionPane.showMessageDialog(null, "Está tudo no lugar!", "Informação", JOptionPane.INFORMATION_MESSAGE)
         }
+    }
+
+
+    override fun move(filePairs: FilePair): Boolean {
+        val result = JOptionPane.showConfirmDialog(
+            null,
+            "Arquivo\n${filePairs.lyricFile.name}\nSerá Movido de\n${filePairs.lyricFile.parentFile}\nPara\n${
+                filePairs.audioFile.parentFile
+            }\ne Renomeado para\n${filePairs.audioFile.nameWithoutExtension}.lrc\njunto a\n${filePairs.audioFile.name}",
+            "Deseja continuar?",
+            JOptionPane.YES_NO_OPTION,
+            JOptionPane.QUESTION_MESSAGE
+        )
+
+        if (result == JOptionPane.CLOSED_OPTION) {
+            exitProcess(0)
+        }
+
+        return result == JOptionPane.YES_OPTION
     }
 }
