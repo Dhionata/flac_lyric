d98f2ae171a83e3a2dafbab296516f1f338ea235
Dhionatã <xiyunagi@gmail.com>
2024-09-23 22:05:05 -0300

    Adicionado opção findMusicWithoutLyricsPair()

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,9 +1,19 @@
+import interfaces.UserInterface
 import services.MusicLyricsService
 import ui.UserInterfaceImpl
 
 fun main() {
     try {
-        MusicLyricsService().organizeMusicAndLyrics()
+        val userInterface: UserInterface = UserInterfaceImpl()
+        val option = userInterface.option()
+        val musicLyricsService = MusicLyricsService()
+        if (option == 0) {
+            musicLyricsService.organizeMusicAndLyrics()
+        } else if (option == 1) {
+            musicLyricsService.findMusicWithoutLyricsPair()
+        } else {
+            throw RuntimeException("this shouldn't happen")
+        }
     } catch (e: Exception) {
         UserInterfaceImpl().showError(e.toString())
     }
--- src/main/kotlin/services/MusicLyricsService.kt
+++ src/main/kotlin/services/MusicLyricsService.kt
@@ -5,6 +5,8 @@ import interfaces.DirectoryService
 import interfaces.LyricFileHandler
 import interfaces.UserInterface
 import ui.UserInterfaceImpl
+import java.io.File
+import kotlin.collections.mutableSetOf
 
 class MusicLyricsService(
     private val directoryService: DirectoryService = DirectoryServiceImpl(),
@@ -38,5 +40,30 @@ class MusicLyricsService(
         lyricFileHandler.handleUnmatchedFiles(musicDirectory, lyricsDirectory)
 
         userInterface.showResult(lyricFileHandler.changedSet, lyricFileHandler.errorSet)
+    }
+
+    fun findMusicWithoutLyricsPair() {
+        val musicDirectory = directoryService.getDirectory("Selecione o diretório das músicas")
+
+        if (!musicDirectory.canWrite()) {
+            userInterface.showError("Diretório $musicDirectory somente leitura!")
+            return
+        }
+
+        val audioFiles = audioFileHandler.getAudioFiles(musicDirectory)
+        val musicsWithoutLyrics = mutableSetOf<String>()
+
+        audioFiles.forEach { audioFile ->
+            val contains = audioFile.parentFile.walk().find { otherFile ->
+                otherFile.name == audioFile.nameWithoutExtension + ".lrc"
+            }
+            if (contains == null) {
+                musicsWithoutLyrics.add(audioFile.nameWithoutExtension)
+            }
+        }
+
+        File("MusicsWithoutLyrics ${musicsWithoutLyrics.hashCode()}.txt").writeText(
+            musicsWithoutLyrics.joinToString("\n")
+        )
     }
 }
