d292ef087844ef530cd3adc7acfbc2879f770f5e
Dhionatã <xiyunagi@gmail.com>
2024-10-28 22:02:18 -0300

    AudioFileHandlerImp() movido para o package "handlers"

---
+++ src/main/kotlin/handlers/AudioFileHandlerImpl.kt
@@ -0,0 +1,13 @@
+package handlers
+
+import interfaces.AudioFileHandler
+import java.io.File
+
+class AudioFileHandlerImpl : AudioFileHandler {
+
+    private val supportedExtensions = listOf("flac", "mp3", "ogg", "wav", "m4a")
+
+    override fun getAudioFiles(musicDirectory: File): Set<File> {
+        return musicDirectory.walk().filter { it.isFile && it.extension in supportedExtensions }.toSet()
+    }
+}
